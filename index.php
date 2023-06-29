32073973ea5586afa6ca13c3788f2ef7fa73f168
lucas <lucasmartinsnara@gmail.com>
2023-06-29 17:27:15 -0300

    criação e estilização do header

--- index.php
+++ index.php
@@ -9,6 +9,26 @@
     <title>LaTurismo</title>
 </head>
 <body>
-    
+    <header>
+        <nav>
+            <div class="principal-nav">
+                <div class="imagem">
+                    <img src="imgs/logo.png" alt="Logo">
+                </div>
+                <div class="list-liks">
+                    <a href="">Explore</a>
+                    <a href="">Booking</a>
+                    <a href="">Tour & Packages</a>
+                    <a href="">Events & Celebrations</a>
+                    <a href="">Medias Rooms</a>
+                    <a href="">Influencers</a>
+                    <a href="">Support</a>
+                </div>
+                <div class="img-selec-moneyAndLanguage">
+                    <img src="imgs/grupo_img_nav.png" alt="lost">
+                </div>
+            </div>
+        </nav>
+    </header>
 </body>
 </html>
