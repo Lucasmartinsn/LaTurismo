5b4ee6daae72c596b4562b064103d94e6f9c3bfa
lucas <lucasmartinsnara@gmail.com>
2023-06-30 10:55:27 -0300

    finalização da estilização da primeira section

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -8,21 +9,24 @@
     <link rel="short icon" href="imgs/logo.png">
     <title>LaTurismo</title>
 </head>
+
 <body>
     <header>
         <nav>
             <div class="principal-nav">
-                <div class="imagem">
-                    <img src="imgs/logo.png" alt="Logo">
-                </div>
-                <div class="list-liks">
-                    <a href="">Explore</a>
-                    <a href="">Booking</a>
-                    <a href="">Tour & Packages</a>
-                    <a href="">Events & Celebrations</a>
-                    <a href="">Medias Rooms</a>
-                    <a href="">Influencers</a>
-                    <a href="">Support</a>
+                <div class="desc">
+                    <div>
+                        <img src="imgs/logo.png" alt="Logo">
+                    </div>
+                    <div class="list-liks">
+                        <a href="">Explore</a>
+                        <a href="">Booking</a>
+                        <a href="">Tour & Packages</a>
+                        <a href="">Events & Celebrations</a>
+                        <a href="">Medias Rooms</a>
+                        <a href="">Influencers</a>
+                        <a href="">Support</a>
+                    </div>
                 </div>
                 <div class="img-selec-moneyAndLanguage">
                     <img src="imgs/grupo_img_nav.png" alt="lost">
@@ -30,5 +34,35 @@
             </div>
         </nav>
     </header>
+
+    <section>
+        <div class="principal_OneSection">
+            <div class="container-img">
+                <img class="img1_oneSection" src="imgs/floresta.png" alt="Floresta">
+                <div class="card-sectionOne">
+                    <h1 class="title-sectionOne">EXPLORE NATURAL <BR /> BEAUTY & WILD LIFE!</h1>
+                    <p class="paragraph-sectionOne"><span id="span-sectionOne">Andhra Pradesh</span>, a Kohinoor of India, enter a city or a <br /> landmark and we'll find the right tours for you </p>
+                    <button class="btn-principal">Learn More</button>
+                </div>
+                <div class="img2_oneSection">
+                    <h1 class="title-sectionOne">Find places on map</h1>
+                    <div id="backgroud">
+                        <img src="imgs/map.png" id="img" alt="">
+                    </div>
+                </div>
+                <div class="card2-sectionOne">
+                    <div class="d-flex">
+                        <h3 class="title-h3">Hotel Booking in APTDC</h3>
+                        <div style="margin-left: 50px;"><img src="imgs/slider.png" alt=""></div>
+                    </div>
+                    <img src="imgs/Search.png" alt="">
+                </div>
+            </div>
+
+        </div>
+    </section>
+
+
 </body>
+
 </html>
